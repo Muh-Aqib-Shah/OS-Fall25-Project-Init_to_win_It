b232aa3927eb95b3e1eaeb5d6e0693474f43e99b
Muh-Aqib-Shah <aqibshah1310@gmail.com>
2025-11-16 09:25:34 -0500

    Added .gitignore

--- llama2c/printfmt.d
+++
@@ -1,2 +0,0 @@
-../llama2c/printfmt.o: ../llama2c/printfmt.c ../llama2c/mini_lib.h \
- /usr/lib/gcc/riscv64-unknown-elf/14.2.0/include/stdarg.h
--- llama2c/sscan.d
+++
@@ -1,2 +0,0 @@
-../llama2c/sscan.o: ../llama2c/sscan.c ../llama2c/mini_lib.h \
- /usr/lib/gcc/riscv64-unknown-elf/14.2.0/include/stdarg.h
